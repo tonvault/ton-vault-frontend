f91462b62bf84e910aa19f9345e6c89bb4e51b6c
shcube <shandin.va@gmail.com>
2023-07-24 14:13:00 +0700

    headers
    (cherry picked from commit 1d34351f700c1ee1ed1ea9332db03dcea3d49690)

--- src/app/api/auth/auth-request/route.ts
+++ src/app/api/auth/auth-request/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import { TonVaultApi } from '@/services/ton-vault-api/ton-vault-api';
+import { headers } from 'next/headers';
 
 export async function GET() {
+    const headersList = headers();
     const result = await TonVaultApi.getAuthPayload();
     return NextResponse.json(result);
 }
